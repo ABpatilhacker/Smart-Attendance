197b7849e8f88ea586e8fba919c990a51a6d210c
Anmol Bhadade <131282176+ABpatilhacker@users.noreply.github.com>
2026-01-08 18:44:07 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB3ytMC77uaEwdqmXgr1t-PN0z3qV_Dxi8",
   authDomain: "smart-attendance-system-17e89.firebaseapp.com",
@@ -9,19 +8,6 @@ const firebaseConfig = {
   appId: "1:168700970246:web:392156387db81e92544a87"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const database = firebase.database();
-
-// Check if user is logged in
-function checkAuth() {
-  auth.onAuthStateChanged(user => {
-    if(!user) window.location.href = "login.html";
-  });
-}
-
-// Logout
-function signOutUser() {
-  auth.signOut().then(() => window.location.href = "login.html");
-}
